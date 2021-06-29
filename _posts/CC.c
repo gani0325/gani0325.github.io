4e4bd507f95d1251fa3b9585d2901574cd941ce7
gani <gani0325@bme.gachon.ac.kr>
2021-06-29 17:19:04 +0900

    your commit message

---
+++ _posts/CC.c
@@ -0,0 +1,10 @@
+#include <stdio.h> //stdio -> standard input output (표준입출력)
+int main(void)
+{
+    printf("Be happy \n");
+    printf("징징이 바보 \n");
+    printf("징징이는 \t 코드를 \t 공부한다 \n");
+    printf("goot\bd\t\chance\n");
+    printf("Cow\rW\a\n");
+    return 0;
+}
--- _posts/ㅇㅇ.py
+++ _posts/ㅇㅇ.py
@@ -1,11 +1,16 @@
-list1 = [1,2]
-int1 = 0
-float1 = 3.14
-set1 = set()
-tuple1 = (2,3)
 
-print(id(list1))
-print(id(int1))
-print(id(float1))
-print(id(set1))
-print(id(tuple1))
+class HelloWorld :
+    def __init__(self) :
+        print("Helllo world! just one more time! ")
+    def __del__(self) :
+        print("Good bye!")
+    def performAverage(self, val1, val2) :
+        average = (val1 + val2) /2.0
+        print("The average of the scores is : ", average)
+
+def main() :
+    world = HelloWorld()
+    score1, score2 = map(int, input("Enter the scores : ").split())
+    world.performAverage(score1, score2)
+
+main()
